73e8cbd52f18ef08d837f2c440d9a443439176bf
Joginder Mikael <jogindermikael@gmail.com>
2025-09-19 11:42:40 +0300

    Added a file to demonstrate user input and arithmetics

---
+++ Test/src/ShoppingCart.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+// This demonstrates the use of user input and arithmetics
+
+public class ShoppingCart {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		String item;
+		 double price;
+		 int quantity;
+		 char currency = '$';
+		 
+		 double total;
+		 
+		 System.out.print("What item would you like to buy? ");
+		 item = scanner.nextLine();
+		 
+		 
+		 System.out.print("What is the price for each ");
+		 price = scanner.nextDouble();
+		 
+		 System.out.print("How many would you like ");
+		 quantity = scanner.nextInt();
+		 
+		 total = price * quantity;
+		 
+		 
+		 
+		scanner.close();
+		
+		System.out.println("\nYou have bought " + quantity + " " + item + "/s");
+		System.out.println("Your Total is " +currency + total );
+		
+		
+	}
+
+}
