91da4505b08b8837226cc30423c6abed83e08963
Joginder Mikael <jogindermikael@gmail.com>
2025-09-19 11:28:14 +0300

    Learnt arithmetics

---
+++ Test/src/Arithmetics.java
@@ -0,0 +1,63 @@
+
+
+//This class is designed to teach arithmetic operators in java
+public class Arithmetics {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 3;
+		
+		
+		//int z;
+		
+		/*
+		z = x + y; //addition
+		z = x - y; //Subtraction
+		z = x * y; //multiplication
+		z = x / y; //devision
+		z = x % y; //modulus, the reminder after division. Useful when checking if a number is even or not
+		
+		
+
+		System.out.println(z);
+		
+		*/
+		
+		//augmented assignment operators
+		
+		// x = x + y;  long version
+	/*	
+		x += y; //shortened
+		x -= y;
+		x *= y;
+		x /= y;
+		x %= y;
+		
+		
+		System.out.println(x);
+	
+		*/
+		
+		//Increment and decrement operators
+		
+		
+		
+	//	x +=1; //this is shortened to x++
+		
+			//	x++; //this is the increment operator
+				
+			//	x--; //decrement ... THIS ARE USED IN LOOPS
+				
+		//		System.out.println(x);
+				
+				
+		//ORDER OF OPERATIONS	[P-E-M-D-A-S]	parentheses, exponents, multiplication, division, addition, substraction
+				
+		
+				double result = 3 + 4 * (7-5)/2.0;
+				
+				System.out.println(result);
+
+	}
+
+}
