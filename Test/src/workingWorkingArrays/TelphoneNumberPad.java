48a623686602e98b4b07822ca9b8bd0360ecadad
Joginder Mikael <jogindermikael@gmail.com>
2025-09-29 19:53:55 +0300

    2D array

---
+++ Test/src/workingWorkingArrays/TelphoneNumberPad.java
@@ -0,0 +1,21 @@
+package workingWorkingArrays;
+
+public class TelphoneNumberPad {
+// 2d array
+	// creating a replica of cellphone number pad
+	public static void main(String[] args) {
+		
+		char[][] telephone = 	{{'1','2','3'},
+								{'4','5','6'},
+								{'7','8','9'},
+								{'*','0','#'}};
+		
+		for(char[] row : telephone) {
+			for(char number : row) {
+				System.out.print(number + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
