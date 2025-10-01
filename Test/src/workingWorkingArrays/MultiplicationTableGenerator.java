3760ad96c788584430c8b84b3a59fb5a17d48f17
Joginder Mikael <jogindermikael@gmail.com>
2025-10-01 18:54:40 +0300

    Working with arrays, table generator

--- Test/src/workingWorkingArrays/MultiplicationTableGenerator.java
+++ Test/src/workingWorkingArrays/MultiplicationTableGenerator.java
@@ -1,10 +1,38 @@
 package workingWorkingArrays;
 
+import java.util.Scanner;
+
 public class MultiplicationTableGenerator {
 	// Multiplication table generator, arrays
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Declare variables
+		int num;
+		int[] table;
+		Scanner scanner = new Scanner(System.in);
+		
+		// user input
+		System.out.print("Please enter the number you want to create a multiplication table for: ");
+		num = scanner.nextInt();
+		
+		//array to store the results
+		table = new int[10];
+		
+		//generate the multiplication table
+		for (int i = 0; i < 10; i++) {
+			table [i] = num * i;
+	
+		}
+		
+		
+		//output
+		System.out.println("Multiplication Table of " + num + ":");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(num + " x " + (i + 1) + " = " + table[i]);
+        }
+		
 
+	scanner.close();
 	}
 
 }
