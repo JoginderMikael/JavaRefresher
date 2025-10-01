4d3414ee0e3dccf3e96ac4861df0b4adf5ef89d5
Joginder Mikael <jogindermikael@gmail.com>
2025-10-01 18:43:29 +0300

    Multiplication table

---
+++ Test/src/workingWorkingArrays/MultiplicationTableGenerator.java
@@ -0,0 +1,10 @@
+package workingWorkingArrays;
+
+public class MultiplicationTableGenerator {
+	// Multiplication table generator, arrays
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
