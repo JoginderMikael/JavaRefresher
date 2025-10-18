efb0b660971a4379d245e8e28a0fce3dfb9574b7
Joginder Mikael <jogindermikael@gmail.com>
2025-10-18 23:30:59 +0300

    Recursive BIG INTEGER

--- Test/src/recurssive.java
+++ Test/src/recurssive.java
@@ -1,8 +1,10 @@
+import java.math.BigInteger;
 
 public class recurssive {
 	
 	//recursive method - Slower O(n) for both space and time complexity.
-	static long sum(long number) {
+	//This is good for small numbers - integers
+	static int sum(int number) {
 		if(number > 0) {
 			number += (sum(number-1));
 			return number;
@@ -13,6 +15,7 @@ public class recurssive {
 	}
 	
 	//non recursive method - Faster O(1) for both space and time complexity
+	//Good for larnge numbers
 	static long sum2(long number) {
 		if(number > 0) {
 			return (number*(number+1))/2;
@@ -23,9 +26,20 @@ public class recurssive {
 		
 	}
 	
+	//handling exceptionally large numbers
+	static BigInteger sum3(BigInteger number) {
+		
+		if(number.compareTo(BigInteger.ZERO) > 0) {
+			return number.multiply(number.add(BigInteger.ONE)).divide(BigInteger.TWO);
+		}else {
+			return BigInteger.ZERO;
+		}
+	}
+	
 	public static void main(String[] args) {
 		long startTime = System.nanoTime();
-		System.out.println(sum2(1000000L));
+		BigInteger number = new BigInteger("100000000000000000");
+		System.out.println(sum3(number));
 		//System.out.println(sum(10009L));
 		long endTime = System.nanoTime();
 		System.out.println("Execution time: " + (endTime - startTime) / 1_000_000.0 + " ms");
