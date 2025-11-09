2fc3abd6d4b9c09c98b1d574a3616aeaa368044c
Joginder Mikael <jogindermikael@gmail.com>
2025-11-09 11:10:59 +0300

    Implementing the use of Lambda expression with the runnable functional interface.

--- Test/src/threading/MultThread.java
+++ Test/src/threading/MultThread.java
@@ -1,41 +1,35 @@
 package threading;
 
-class A implements Runnable {
-	public void run() {
-		for (int i = 1; i <= 100; i++) {
-			System.out.println("From A: " + i);
-			
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				System.out.println(e);
-			}
-			
-		}
-	}
-}
-
-class B implements Runnable {
-	public void run() {
-		for (int i = 1; i <= 100; i++) {
-			System.out.println("From B: " + i);
-			
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				System.out.println(e);
-			}
-			
-		}
-	}
-}
-
 public class MultThread {
 	
 
 	public static void main(String[] args) {
-		Runnable threadA = new A();
-		Runnable threadB = new B();
+		//Since runnable is a functional interface, we can use lambda expressions to define the run method.
+		Runnable threadA = () -> {
+			for (int i = 1; i <= 100; i++) {
+				System.out.println("From A: " + i);
+				
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					System.out.println(e);
+				}
+				
+			}
+		};
+		
+		Runnable threadB = () ->  {
+			for (int i = 1; i <= 100; i++) {
+				System.out.println("From B: " + i);
+				
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					System.out.println(e);
+				}
+				
+			}
+		};
 		
 		Thread t1 = new Thread(threadA);
 		Thread t2 = new Thread(threadB);
