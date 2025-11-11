1c36cc2726ebf790e60746f867491aa661a11a22
Joginder Mikael <jogindermikael@gmail.com>
2025-11-11 21:26:45 +0300

    UDP Socket

--- Test/src/socketUDP/UDPClient.java
+++ Test/src/socketUDP/UDPClient.java
@@ -2,6 +2,7 @@ package socketUDP;
 
 public class UDPClient {
 
+	//The client sent values to the server and the server returns the square of the values
 	public static void main(String[] args) {
 		
 
--- Test/src/socketUDP/UDPServer.java
+++ Test/src/socketUDP/UDPServer.java
@@ -2,6 +2,7 @@ package socketUDP;
 
 public class UDPServer {
 
+	//
 	public static void main(String[] args) {
 		
 
